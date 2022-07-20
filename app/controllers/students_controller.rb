53ea3a64375a4639a7bc3cbff3c79587046a5f46
desmond <desmond@desmonds-MacBook-Pro.local>
2022-07-20 11:06:02 +0300

    task complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,11 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  
+    def show
+      student = Student.find(params[:id])
+      render json: student
+    end
+  end
 
-end
+
